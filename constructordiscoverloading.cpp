787388b379bbcdea5833088a714eae5b13274d7a
ZidaneAlhakim <zidanealhakim179@gmail.com>
2025-05-13 16:15:37 +0700

    membuat kode eksekusi

--- constructordiscoverloading.cpp
+++ constructordiscoverloading.cpp
@@ -10,5 +10,44 @@ public:
     mahasiswa();
     mahasiswa(int);
     mahasiswa(string);
-    mahasiswa(int nim, string nama);
+    mahasiswa(int inim, string inama);
+    void cetak();
 };
+
+mahasiswa::mahasiswa() {}
+
+mahasiswa::mahasiswa(int inim)
+{
+    nim = inim; //definisi hanya nim
+}
+
+mahasiswa::mahasiswa(string inama)
+{
+    nama = inama; //definisi hanya nama
+}
+
+mahasiswa::mahasiswa(int inim, string inama)
+{
+    nim = inim;
+    nama = inama; // definisi hanya nim nama
+}
+
+void mahasiswa::cetak()
+{
+    cout << endl << "nim = " << endl;
+    cout << "nama = " << nama << endl;
+}
+
+int main() 
+{
+    mahasiswa mhs1(202401);           // hanya nim
+    mahasiswa mhs2("Zidane");         // hanya nama
+    mahasiswa mhs3(202401, "Zidane"); // nim dan nama
+
+    //tampilkan nilai
+    cout << "mahasiswa 1 : "; mhs1.cetak();
+    cout << "mahasiswa 2 : "; mhs2.cetak();
+    cout << "mahasiswa 3 : "; mhs3.cetak();
+
+    return 0;
+}
