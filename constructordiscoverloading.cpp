6d64d983d46657ae4ea5c28df5c5ec9210e7bd58
ZidaneAlhakim <zidanealhakim179@gmail.com>
2025-05-13 16:02:00 +0700

    membuat class mahasiswa

--- constructordiscoverloading.cpp
+++ constructordiscoverloading.cpp
@@ -1,3 +1,14 @@
 #include <iostream>
 using namespace std;
 
+class mahasiswa 
+{
+private: 
+    int nim;
+    string nama;
+public:
+    mahasiswa();
+    mahasiswa(int);
+    mahasiswa(string);
+    mahasiswa(int nim, string nama);
+};
